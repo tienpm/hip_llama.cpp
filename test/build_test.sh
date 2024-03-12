ee11dda80847c8361d394e9e5b49fd338ea3e877
xuananh000 <xuananhbka@gmail.comcd>
2024-03-13 03:31:11 +0900

    Revert "add test tensor core" This reverts commit eca178d545afdc88011de44b1f919ec24ebaaf05.

--- test/build_test.sh
+++ test/build_test.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-
-hipcc -Wall -O1 -std=c++17 --offload-arch=gfx908 -lstdc++ -lm -fopenmp test_ten.cpp ../src/thaBLAS.cpp ../src/thaDNN.cpp ../src/utils.cpp ../src/seq.cpp -I../include -I/opt/rocm/include/hipblas -o test_ten_run
-srun -p EM  --gres=gpu:1 ./test_ten_run
-
-
 # hipcc -Wall -O1 -std=c++17 --offload-arch=gfx908 -lstdc++ -lm -fopenmp thaBLAS.test.cpp ../src/thaBLAS.cpp                  ../src/utils.cpp ../src/seq.cpp -I../include -I/opt/rocm/include/hipblas -o test_thaBLAS_run
 # srun -p EM  --gres=gpu:1 ./test_thaBLAS_run
 
 
-# hipcc -Wall -O1 -std=c++17 --offload-arch=gfx908 -lstdc++ -lm -fopenmp thaDNN.test.cpp ../src/thaBLAS.cpp ../src/thaDNN.cpp ../src/utils.cpp ../src/seq.cpp -I../include -I/opt/rocm/include/hipblas -o test_thaDNN_run
-# srun -p EM  --gres=gpu:1 ./test_thaDNN_run
+hipcc -Wall -O1 -std=c++17 --offload-arch=gfx908 -lstdc++ -lm -fopenmp thaDNN.test.cpp ../src/thaBLAS.cpp ../src/thaDNN.cpp ../src/utils.cpp ../src/seq.cpp -I../include -I/opt/rocm/include/hipblas -o test_thaDNN_run
+srun -p EM  --gres=gpu:1 ./test_thaDNN_run
 
 
 # srun -p EM  --gres=gpu:4 ./test_thaBLAS_run
